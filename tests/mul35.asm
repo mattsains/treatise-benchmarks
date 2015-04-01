5c9845c6a0b1403521ef509cd4b2231b297440af
Matthew Sainsbury <matthew@sainsbury.za.net>
2015-04-01 17:14:02 +0200

    Added a test program, and stubs for all our benchmarks

---
+++ tests/mul35.asm
@@ -0,0 +1,23 @@
+; Calculates the sum of all multiples of 3 or 5 under 1000
+; This program should end with r5 = 0x38ed0 = 233168
+movc r1, 1
+movc r5, 0
+
+loop:
+    mov r2, r1
+    divc r1, 3
+    mov r1, r2
+    jcmpc r0, 0, acc, acc, cont
+    cont:
+    divc r1, 5
+    mov r1, r2
+    jcmpc r0, 0, acc, acc, reject
+    acc:
+    add r5, r1
+    reject:
+    addc r1, 1
+
+    jcmpc r1, 1000, loop, end, end
+
+end:
+    ;Don't know how to print a value to the screen
