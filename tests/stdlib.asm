ab0ee432648c6a8995aaf263c1584d61f8e21cf7
Matthew Sainsbury <matthew@sainsbury.za.net>
2015-04-10 11:01:30 +0200

    Neatened stdlib by adding local labels

--- tests/stdlib.asm
+++ tests/stdlib.asm
@@ -1,5 +1,4 @@
-;;;;;;;;;;;;;;;;;;;;;;;;
-; Calculates x^y, y>=0
+;Calculates x^y, y>=0
 function pow
   int x
   int y
@@ -7,18 +6,16 @@ function pow
 getl r1, pow.x ;r1: x
 movc r0, 1 ;r0: 1
 getl r2, pow.y ;r2: y
-pow_loop:
-    jcmpc r2, 0, eloop, eloop, loopnext
-    loopnext:
+.loop:
+    jcmpc r2, 0, .eloop, .eloop, .next
+    .next:
     mul r0, r1
     addc r2, -1
-    jmp pow_loop
-eloop:
+    jmp .loop
+.eloop:
 ret
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Takes in an integer,
-; returns a \0-terminated string in a byte buffer
+; Takes in an integer, returns a string in a byte buffer
 function i_to_s
   int i
   int ten
@@ -30,14 +27,14 @@ newa r0, r1 ;r0: output
 getl r1, i_to_s.i ;r1: i
 movc r2, 0 ;r2: index into output
 
-jcmpc r1, 0, i_to_s_next, i_to_s_positive, i_to_s_positive
-i_to_s_next:
+jcmpc r1, 0, .next, .positive, .positive
+.next:
 movc r3, 45 ;ASCII minus
 setb r0, r2, r3
 xor r1, r2 ;just using r2=0 to invert r1
 addc r1, 1
 addc r2, 1
-i_to_s_positive:
+.positive:
 movc r3, 18 ;first possible digit of int64
 movc r4, 10
 setl i_to_s.ten, r4
@@ -55,71 +52,68 @@ mov r0, r1
 ;r4: undefined
 ;r5: output
 mov r4, r0
-prestrloop:
+.preloop:
     div r4, r3 ;r4: value of nth digit
-    jcmpc r4, 0, prenext, prenext, endpre
-    prenext:
+    jcmpc r4, 0, .prenext, .prenext, .endpre
+    .prenext:
     mov r4, r0
     divc r3, 10
-    jcmpc r3, 0, prefail, prefail, prestrloop
+    jcmpc r3, 0, .prefail, .prefail, .preloop
 
-prefail:
+.prefail:
     ;this means the number is zero
     movc r4, 48
     setb r5, r2, r4
-    jmp i_to_s_end
+    jmp .end
 
-endpre:
+.endpre:
     ;we've cleared zeros
     addc r4, 48
     setb r5, r2, r4
     addc r2, 1
     mov r4, r0
     divc r3, 10
-    jcmpc r3, 0, i_to_s_end, i_to_s_end, strloop
+    jcmpc r3, 0, .end, .end, .loop
       
-strloop:
+.loop:
     div r4, r3 ;r4: value of nth digit
     addc r4, 48 ;ASCII 0
     setb r5, r2, r4
     addc r2, 1
     mov r4, r0
     divc r3, 10
-    jcmpc r3, 0, i_to_s_end, i_to_s_end, strloop
-i_to_s_end:
+    jcmpc r3, 0, .end, .end, .loop
+.end:
 movc r4, 0
 setb r5, r2, r4 ;\0
 mov r0, r5
 ret
 
-;;;;;;;;;;;;;;;;;;;;;;;
-; Takes as input two string buffers
-; returns a string buffer that is the
-; concatenation of the two
+
 function strconcat
   ptr a
   ptr b
 
 getl r1, strconcat.a ;r1: a
 movc r0, 0 ;r0: length
-strconcat_cloop1:
+.cloop1:
     getb r2, r1, r0
-    jcmp r2, 0, strconcat_cloop1e, strconcat_cloop1e, strconcat_cloop1c
-    strconcat_cloop1c:
+    jcmp r2, 0, .cloop1e, .cloop1e, .cloop1c
+    .cloop1c:
     addc r0, 1
-    jmp strconcat_cloop1
-strconcat_cloop1e:
-mov r5, r0 ;store length of 0 for later
+    jmp .cloop1
+.cloop1e:
+mov r5, r0 ;store length of a for later
 getl r1, strconcat.b ;r1: b
 movc r2, 0 ;r2: index
-strconcat_cloop2:
+.cloop2:
     getb r4, r1, r2
-    jcmp r4, 0, strconcat_cloop2e, strconcat_cloop2e, strconcat_cloop2c
-    strconcat_cloop2c:
+    jcmp r4, 0, .cloop2e, .cloop2e, .cloop2c
+    .cloop2c:
     addc r0, 1
     addc r2, 1
-    jmp strconcat_cloop2
-strconcat_cloop2e:
+    jmp .cloop2
+.cloop2e:
 addc r0, 1 ;add \0
 mov r3, r0
 addc r3, 7
@@ -138,28 +132,28 @@ mov r1, r5           ;r1: len(a)
                      ;r2: len(b)
 getl r3, strconcat.a ;r3: a
 movc r4, 0           ;r4: index into a
-strconcat_mloop1:
-    jcmpc r1, 0, strconcat_mloop1e, strconcat_mloop1e, strconcat_mloop1c
-    strconcat_mloop1c:
+.mloop1:
+    jcmpc r1, 0, mloop1e, .mloop1e, .mloop1c
+    .mloop1c:
     getb r5, r3, r4
     setb r0, r4, r5
     addc r1, -1
     addc r4, 1
-    jmp strconcat_mloop1
-strconcat_mloop1e:
+    jmp .mloop1
+.mloop1e:
 getl r3, strconcat.b
 mov r1, r4
 movc r4, 0
-strconcat_mloop2:
-    jcmpc r2, 0, strconcat_mloop2e, strconcat_mloop2e, strconcat_mloop2c
-    strconcat_mloop2c:
+.mloop2:
+    jcmpc r2, 0, .mloop2e, .mloop2e, .mloop2c
+    .mloop2c:
     getb r5, r3, r4
     setb r0, r1, r5
     addc r4, 1
     addc r1, 1
     addc r2, -1
-    jmp strconcat_mloop2
-strconcat_mloop2e:
+    jmp .mloop2
+.mloop2e:
 movc r5, 0
 setb r0, r1, r5
 ret
