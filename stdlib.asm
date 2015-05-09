0bbf9bb47cbcfb64e8b6f7da52c2917dbec8bfdd
Douglas Bentley <dee.bent.lee@gmail.com>
2015-05-09 17:00:16 +0200

    Fixed str to int

--- stdlib.asm
+++ stdlib.asm
@@ -1,4 +1,8 @@
-;Calculates x^y, y>=0
+; int pow(int x, int y)
+; =====================
+; Calculates x^y, y>=0
+; result in r0, r5 preserved
+  
 function pow
   int x
   int y
@@ -13,14 +17,18 @@ getl r2, pow.y ;r2: y
     jmp .loop
 .eloop:
 ret
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Takes in an integer, returns a string in a byte buffer
+
+; buffer i_to_s(int i)
+; ====================
+; Returns i represented as a string in a byte buffer
+; result in r0, r5 clobbered
+  
 function i_to_s
   int i
   int ten
   int pow
 
-movc r1, 21 ;max digits in int64_t + 2 (negatives+\0) /8
+movc r1, 21 ;max digits in int64_t + 2 (negatives+\0)
 newa r0, r1 ;r0: output
 
 getl r1, i_to_s.i ;r1: i
@@ -89,7 +97,108 @@ setb r5, r2, r4 ;\0
 mov r0, r5
 ret
 
+; int s_to_i(buffer s)
+; ====================
+; Returns s parsed as an int
+; result in r0, r5 clobbered
+  
+function s_to_i
+  ptr s
+  int sign
+  int ten
+  int exp
+  int acc
+  ; legal numbers must match the form:
+  ; [ws][sign][digits][ws]
+  ; ws = TAB (9) or Space (32)
+  ; sign = + (43) or - (45)
+  ; up to 18 digits - this simplifies the code a lot at the expense of
+  ; losing some huge numbers you can't even read
+  movc r5, 0
+  setl s_to_i.acc, r5
+  movc r3, 1
+  setl s_to_i.sign, r3
+  movc r3, 10
+  setl s_to_i.ten, r3
+  getl r1, s_to_i.s
+  movc r0, -1
+.skipws:
+  addc r0, 1
+  getb r2, p1, r0
+  jcmpc r2, 11, .error, .skipws, $ 
+  jcmpc r2, 32, .error, .skipws, $
+  ;ascii < 32 are all control chars except 11 and 0
+  
+  ;check for minus or plus
+  movc r4, 0 ;digit count = 0
+  jcmpc r2, 43, .error, .digit, $
+  jcmpc r2, 45, .error, .minus, $
+  addc r0, -1 ;reset this to hand over to .digit
+  jmp .digit
+  
+.minus:
+  movc r3, -1
+  setl s_to_i.sign, r3
+.digit:
+  addc r0, 1
+  getb r2, p1, r0
+  jcmpc r2, 48, .isws, $, $
+  jcmpc r2, 57, $, $, .error
+  addc r4, 1 ;digit count += 1
+  jcmpc r4, 19, .digit, .error, .error
+  ;hitting a 19th digit is an error
 
+.isws:
+  jcmpc r4, 0, .error, .error, $
+  ;ensure at least one digit (prevent [ws][sign][null])
+.isws2:
+  jcmpc r2, 0, .error, .convert0, $
+  jcmpc r2, 11, .error, .endws, $
+  jcmpc r2, 32, .error, .endws, .error  
+.endws:
+  addc r0, 1
+  addc r5, 1 ;keep count of how many chars after digits
+  getb r2, p1, r0
+  jcmpc r2, 0, .error, .convert, .isws2
+
+.convert0:
+  addc r5, 1 ;ignore null
+.convert:
+  ;r4 is how many digits we have
+  ;r3 is a counter for 10's exponent
+  ;r0 is pos in string
+  ;r1 is the string
+  ;r5 is num chars past the last digit we are
+  sub r0, r5 ;r0 is now the pos of the last digit...
+  mov r5, r0 ;move pos out of r0 for fn call
+  movc r3, -1
+.convertloop:
+  addc r4, -1
+  addc r3, 1
+  getb r2, p1, r5
+  addc r2, -48 ;ascii offset
+  addc r5, -1
+  ;calculation r2*(10^r3)
+  setl s_to_i.exp, r3
+  call pow, s_to_i.ten, 2
+  mul r2, r0  
+  ;accumulate
+  getl r0, s_to_i.acc
+  add r0, r2
+  setl s_to_i.acc, r0
+  jcmpc r4, 0, .error, .done, .convertloop
+.error:
+
+.done:
+  getl r3, s_to_i.sign
+  mul r0, r3 ;change sign accordingly
+ret  
+
+; buffer strconcat(buffer a, buffer b)
+; ====================================
+; Returns concatenation of b onto the end of a
+; result in r0, r5 clobbered
+  
 function strconcat
   ptr a
   ptr b
@@ -113,15 +222,12 @@ movc r2, 0 ;r2: index
     jmp .cloop2
 .cloop2e:
 addc r0, 1 ;add \0
-mov r3, r0
-addc r3, 7
-shrc r3, 3 ;r=(r0+7)/64
-newa r4, r3
+newa r4, r0
 ;recap:
 ;r0: length of new array
 ;r1: b
 ;r2: length of b
-;r3: size of output
+;r3: nothing
 ;r4: output
 ;r5: length of a
 ;rearrange for ease of use
