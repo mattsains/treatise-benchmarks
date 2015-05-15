a5302b2970a8ba7975b39349802f3fff82d70d58
Matthew Sainsbury <matthew@sainsbury.za.net>
2015-05-15 12:33:01 +0200

    Format and increased accuracy of RNG for fasta

--- benchmarks/fasta.c
+++ benchmarks/fasta.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 long seed = 42;
-// max is the number you want * 10000
-int random(int max)
+// max is the number you want * 1000000
+int random(long max)
 {
   seed = (seed * 3877 + 29573) % 139968;
-  return (seed * max) / 1399680000;
+  return (seed * max) / 139968000000;
 }
 
 struct Frequency
@@ -26,7 +26,7 @@ void makeCumulative (struct Frequency frequencies[], int length)
 
 char selectRandom (struct Frequency frequencies[], int length)
 {
-  int r = random(10000000); //1000
+  int r = random(10000000000); //10000
 
   for (int i=0; i<length; i++)
     if (r<frequencies[i].p)
@@ -45,25 +45,26 @@ void makeRandomFasta (struct Frequency frequencies[], int length, int n)
     else
       m = 60;
 
-    if (100 - index < m) {
-      buffer[index] = '\0';
-      printf(buffer);
-      index = 0;
-    }
-
     for (int i=0; i<m; i++) {
       buffer[index] = selectRandom(frequencies, length);
       index++;
     }
 
-    buffer[index] = '\n';
-    index++;
+    if (1024 - index + 60 < m) {
+      buffer[index] = '\0';
+      printf("%s\n", buffer);
+      index = 0;
+    }
+    else if (n - 60 > 0) {
+      buffer[index] = '\n';
+      index++;
+    }
     n-=60;
   }
 
   if (index!=0) {
     buffer[index] = '\0';
-    printf(buffer);
+    printf("%s\n", buffer);
   }
 }
 
@@ -81,12 +82,7 @@ void makeRepeatFasta(char alu[], int length, int n)
       m = n;
     else
       m = 60;
-
-    if (length - index < m) {
-      buffer[index] = '\0';
-      printf(buffer);
-      index = 0;
-    }
+    
     for (int i=0; i<m; i++) {
       if (k == kn)
         k = 0;
@@ -96,47 +92,54 @@ void makeRepeatFasta(char alu[], int length, int n)
       k++;
     }
 
-    buffer[index] = '\n';
-    index++;
+    if (1024 - index + 60 < m) {
+      buffer[index] = '\0';
+      printf("%s\n", buffer);
+      index = 0;
+    }
+    else if (n - 60 > 0) {
+      buffer[index] = '\n';
+      index++;
+    }
     n -= 60;
   }
 
   if (index != 0) {
     buffer[index] = '\0';
-    printf(buffer);
+    printf("%s\n", buffer);
   }
 }
 
 int main()
 {
   struct Frequency HomoSapiens[4];
-  HomoSapiens[0].c = 'a'; HomoSapiens[0].p = 303;
-  HomoSapiens[1].c = 'c'; HomoSapiens[1].p = 198;
-  HomoSapiens[2].c = 'g'; HomoSapiens[2].p = 198;
-  HomoSapiens[3].c = 't'; HomoSapiens[3].p = 302;
+  HomoSapiens[0].c = 'a'; HomoSapiens[0].p = 3030;
+  HomoSapiens[1].c = 'c'; HomoSapiens[1].p = 1980;
+  HomoSapiens[2].c = 'g'; HomoSapiens[2].p = 1975;
+  HomoSapiens[3].c = 't'; HomoSapiens[3].p = 3015;
   struct Frequency IUB[15];
-  IUB[0].c = 'a'; IUB[0].p = 270;
-  IUB[1].c = 'c'; IUB[1].p = 120;
-  IUB[2].c = 'g'; IUB[2].p = 120;
-  IUB[3].c = 't'; IUB[3].p = 270;
-  IUB[4].c = 'B'; IUB[4].p = 20;
-  IUB[5].c = 'D'; IUB[5].p = 20;
-  IUB[6].c = 'H'; IUB[6].p = 20;
-  IUB[7].c = 'K'; IUB[7].p = 20;
-  IUB[8].c = 'M'; IUB[8].p = 20;
-  IUB[9].c = 'N'; IUB[9].p = 20;
-  IUB[10].c = 'R'; IUB[10].p = 20;
-  IUB[11].c = 'S'; IUB[11].p = 20;
-  IUB[12].c = 'V'; IUB[12].p = 20;
-  IUB[13].c = 'W'; IUB[13].p = 20;
-  IUB[14].c = 'Y'; IUB[14].p = 20;
+  IUB[0].c = 'a'; IUB[0].p = 2700;
+  IUB[1].c = 'c'; IUB[1].p = 1200;
+  IUB[2].c = 'g'; IUB[2].p = 1200;
+  IUB[3].c = 't'; IUB[3].p = 2700;
+  IUB[4].c = 'B'; IUB[4].p = 200;
+  IUB[5].c = 'D'; IUB[5].p = 200;
+  IUB[6].c = 'H'; IUB[6].p = 200;
+  IUB[7].c = 'K'; IUB[7].p = 200;
+  IUB[8].c = 'M'; IUB[8].p = 200;
+  IUB[9].c = 'N'; IUB[9].p = 200;
+  IUB[10].c = 'R'; IUB[10].p = 200;
+  IUB[11].c = 'S'; IUB[11].p = 200;
+  IUB[12].c = 'V'; IUB[12].p = 200;
+  IUB[13].c = 'W'; IUB[13].p = 200;
+  IUB[14].c = 'Y'; IUB[14].p = 200;
   char ALU[] = "GGCCGGGCGCGGTGGCTCACGCCTGTAATCCCAGCACTTTGGGAGGCCGAGGCGGGCGGATCACCTGAGGTCAGGAGTTCGAGACCAGCCTGGCCAACATGGTGAAACCCCGTCTCTACTAAAAATACAAAAATTAGCCGGGCGTGGTGGCGCGCGCCTGTAATCCCAGCTACTCGGGAGGCTGAGGCAGGAGAATCGCTTGAACCCGGGAGGCGGAGGTTGCAGTGAGCCGAGATCGCGCCACTGCACTCCAGCCTGGGCGACAGAGCGAGACTCCGTCTCAAAAA"; //has length 287
   
 
   makeCumulative(HomoSapiens, 4);
   makeCumulative(IUB, 15);
 
-  char* text = ">ONE Homo Sapiens alu";
+  char* text = ">ONE Homo sapiens alu";
   printf("%s\n", text);
   makeRepeatFasta(ALU, 287, 2000); //n*2 = 2000
 
