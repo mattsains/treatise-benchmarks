9a82ec6bd7353a4ed18f3dc2f31ae07e0a5a117d
bentles <dee.bent.lee@gmail.com>
2015-09-28 15:34:50 +0200

    ack-fast remove load sto
    also renamed registers

--- benchmarks/ackermann-fast.asm
+++ benchmarks/ackermann-fast.asm
@@ -1,33 +1,31 @@
 function ackermann
   ;hardcoded inputs
-  movc r0, 4 ;m
-  movc r5, 1 ;n
+  movc r1, 4 ;m
+  movc r0, 1 ;n
   call A, 0, 0
-  ;r5 now has the result of the ackermann function
+  ;r0 now has the result of the ackermann function
   ;should be 65533 for (4,1)
   ; takes about 3 minutes
-  print r5
+  print r0
 ret
  
-function A ;r0 is m, r5 is n
-  jcmpc r0, 0, c1, c1, c23
+function A ;r1 is m, r0 is n
+  jcmpc r1, 0, c1, c1, c23
 c1:
-  addc r5, 1
+  addc r0, 1
 ret
 c23:
-  jcmpc r5, 0, c2, c2, c3
+  jcmpc r0, 0, c2, c2, c3
 c2:
-  addc r0, -1
-  movc r5, 1
+  addc r1, -1
+  movc r0, 1
   call A, 0, 0
 ret
 c3:
-  addc r5, -1
-  mov r3, r0 ; save r0 (m)
+  addc r0, -1
   call A, 0, 0
-  ;r5 now result
-  addc r3, -1
-  ;restore r0
-  mov r0, r3
+  ;r0 now result
+  addc r1, -1
+  ;restore r1
   call A, 0, 0
 ret
